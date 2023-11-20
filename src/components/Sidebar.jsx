a92e1211cb065ed91d61cae3c9af6ead9ea5a5da
Mathieu Etourneaud <mathieu.etourneaud@lapiscine.pro>
2023-11-20 16:42:49 +0100

    Add some modifications on the sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,19 +2,16 @@ import "./Sidebar.css"
 
 function Sidebar({currentPage, text}) {
     return(
-        <div className="container">
-            <div className="sidebar">
-                {currentPage == "HomePage" && (
-                    <h2>Le titre du site</h2>
-                )}
-                {currentPage == "ProductsPage" && (
-                    <h2>Vous consultez la liste des produits</h2>
-                )}
-                {currentPage == "DetailsPage" && (
-                    <h2>{`Vous consultez le produit ${text}`}</h2>
-                )}
-                
-            </div>
+        <div className="sidebar">
+            {currentPage == "HomePage" && (
+                <h2>Le titre du site</h2>
+            )}
+            {currentPage == "ProductsPage" && (
+                <h2>Vous consultez la liste des produits</h2>
+            )}
+            {currentPage == "DetailsPage" && (
+                <h2>{`Vous consultez le produit ${text}`}</h2>
+            )}
         </div>
     )
 }
